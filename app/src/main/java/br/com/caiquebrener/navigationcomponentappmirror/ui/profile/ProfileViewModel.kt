8b4b8d4e2c4a09aceef7fc51e37b0832b3df4036
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-04-03 01:13:31 -0300

    Profile Data

---
+++ app/src/main/java/br/com/caiquebrener/navigationcomponentappmirror/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package br.com.caiquebrener.navigationcomponentappmirror.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
